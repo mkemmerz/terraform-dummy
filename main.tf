4f3e6db73b806934bb8b53bafec7c808d1dcbdbc
Michael Kemmerzell <46575727+mkemmerz@users.noreply.github.com>
2023-01-30 12:50:50 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       version = "~> 3.4.0"
-      source  = "registry.example.org/namespace/azurerm"
+      source  = "hashicorp/azurerm"
     }
   }
 }
